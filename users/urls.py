7068b28c86df2e43a34f766cf5cb3e99b154c385
xwp <xwp_fullstack@163.com>
2019-03-07 18:09:10 +0800

    添加新路由

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 
-from . import views
 
-# urlpatterns是被django自动识别的路由列表变量
+from users import views
+
 urlpatterns = [
-            # 每个路由信息都需要使用url函数来构造
-                # url(路径, 视图)
-                    url(r'^index/$', views.index),
-                    ]
+    # 配置子路由
+    url(r'^index/', views.index),
+]
